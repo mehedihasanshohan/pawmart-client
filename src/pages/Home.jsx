9b7676a1b0410352a5ba5f54dfd47b699c366e65
mehedi hasan <mhshohan04@gmail.com>
2025-11-09 00:02:26 +0600

    added two relavent extra section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Slider from '../components/Slider'
+import Adopt from '../components/Adopt'
 
 const Home = () => {
   return (
     <div>
       <Slider></Slider>
+      <Adopt></Adopt>
     </div>
   )
 }
